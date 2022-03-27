018d1f14f4548d4f445a215d8d850dc61eb462a9
raidhqtrnnd <90620651+raidhqtrnnd@users.noreply.github.com>
2022-03-28 06:54:54 +0700

    2,15-17

--- src/server/server.js
+++ src/server/server.js
@@ -13,114 +13,122 @@ app.get("/login", (req, res) => {
 
 // Poin 2
 app.get("/logout", (req, res) => {
-  res.send("Login jika ingin masuk kembali - poin 2 ");
+  res.send("Berhasil Log out");
 });
 
 // Poin 3
-app.get("admin/rps/lihat", (req, res) => {
-  res.send("Halaman Admin menambahkan dosen - poin 3");
+app.post("/admin/rps/tambah", (req, res) => {
+  let pesan = {
+    message: "Tambahkan Dosen :",
+    Dosen: {
+      "Nama Dosen": "Husnil Kamil, MT.",
+      NIP: "198201182008121002",
+      "Status Dosen": "Dosen Sistem Informasi",
+      "Mata Kuliah": ["Pemprograman Web", "Struktur Data"],
+    },
+  };
+
+  res.json(pesan);
+  console.log("Berhasil Menambahkan Dosen ");
 });
 
 //Poin 4
-app.get("/admin/rps/lihatRPS", (req, res) => {
-
+app.get("/admin/rps/lihat", (req, res) => {
   let pesan = {
-    message : "Berikut Laporan Dari RPS :",
-    "Program Studi" : "Sistem Informasi",
-    "Fakultas" : "Teknologi Informasi",
+    message: "Berikut Laporan Dari RPS :",
+    "Program Studi": "Sistem Informasi",
+    Fakultas: "Teknologi Informasi",
     "RPS yang menggunakan project based": {
-      "Pertemuan" : ['P3','P4','P5','P6','P7'],
+      Pertemuan: ["P3", "P4", "P5", "P6", "P7"],
     },
     "Mata Kuliah": {
       "Mata Kuliah": "Pemrograman Web",
-      "Bobot": "3 SKS",
-      "semester": "4",
+      Bobot: "3 SKS",
+      semester: "4",
     },
   };
-  res.json(pesan)
-  console.log("Berhasil Menampilkan ")
+  res.json(pesan);
+  console.log("Berhasil Menampilkan ");
 });
 
 //Poin 5
 app.get("/admin/rps/cetak", (req, res) => {
   let pesan = {
-    message : "Berikut Laporan Dari RPS Yang Akan Dicetak:",
-    "Program Studi" : "Sistem Informasi",
-    "Fakultas" : "Teknologi Informasi",
+    message: "Berikut Laporan Dari RPS Yang Akan Dicetak:",
+    "Program Studi": "Sistem Informasi",
+    Fakultas: "Teknologi Informasi",
     "RPS yang menggunakan project based": {
-      "Pertemuan" : ['P3','P4','P5','P6','P7'],
+      Pertemuan: ["P3", "P4", "P5", "P6", "P7"],
     },
     "Mata Kuliah": {
       "Mata Kuliah": "Pemograman Web",
-      "Bobot": "3 SKS",
-      "semester": "4",
+      Bobot: "3 SKS",
+      semester: "4",
     },
   };
-  res.json(pesan)
-  console.log("Berhasil Mencetak RPS")
+  res.json(pesan);
+  console.log("Berhasil Mencetak RPS");
 });
 
 //Poin 6
 app.get("/dosen/rps/tambah1", (req, res) => {
   let pesan = {
-    message : "Tambahkan RPS Untuk Mata Kuliah:",
-    "Program Studi" : "Sistem Informasi",
-    "Fakultas" : "Teknologi Informasi",
+    message: "Tambahkan RPS Untuk Mata Kuliah:",
+    "Program Studi": "Sistem Informasi",
+    Fakultas: "Teknologi Informasi",
     "RPS yang menggunakan project based": {
-      "Pertemuan" : [],
+      Pertemuan: [],
     },
     "Mata Kuliah": {
       "Mata Kuliah": " ",
-      "Bobot": " ",
-      "semester": " ",
+      Bobot: " ",
+      semester: " ",
     },
   };
-  res.json(pesan)
-  console.log("Berhasil Menambahkan RPS ")
+  res.json(pesan);
+  console.log("Berhasil Menambahkan RPS ");
 });
 
 //Poin 7
 app.get("/dosen/rps/ubah", (req, res) => {
   let pesan = {
-    message1 : "Berikut Laporan Dari RPS :",
-    "Program Studi" : "Sistem Informasi",
-    "Fakultas" : "Teknologi Informasi",
+    message1: "Berikut Laporan Dari RPS :",
+    "Program Studi": "Sistem Informasi",
+    Fakultas: "Teknologi Informasi",
     "RPS yang menggunakan project based": {
-      "Pertemuan" : ['P3','P4','P5','P6','P7'],
+      Pertemuan: ["P3", "P4", "P5", "P6", "P7"],
     },
     "Mata Kuliah": {
       "Mata Kuliah": "Pemograman Web",
-      "Bobot": "3 SKS",
-      "semester": "4",
+      Bobot: "3 SKS",
+      semester: "4",
     },
-    message2 : "Pilih Bagian RPS yang akan diubah"
+    message2: "Pilih Bagian RPS yang akan diubah",
   };
 
-  res.json(pesan)
-  console.log("Berhasil Mengubah RPS ")
+  res.json(pesan);
+  console.log("Berhasil Mengubah RPS ");
 });
 
 //Poin 8
 app.get("/dosen/rps/revisi", (req, res) => {
   let pesan = {
-    message1 : "Berikut Laporan Dari RPS :",
-    "Program Studi" : "Sistem Informasi",
-    "Fakultas" : "Teknologi Informasi",
+    message1: "Berikut Laporan Dari RPS :",
+    "Program Studi": "Sistem Informasi",
+    Fakultas: "Teknologi Informasi",
     "RPS yang menggunakan project based": {
-      "Pertemuan" : ['P3','P4','P5','P6','P7'],
+      Pertemuan: ["P3", "P4", "P5", "P6", "P7"],
     },
     "Mata Kuliah": {
       "Mata Kuliah": "Pemograman Web",
-      "Bobot": "3 SKS",
-      "semester": "4",
+      Bobot: "3 SKS",
+      semester: "4",
     },
-    message2 : "Pilih Bagian RPS yang akan direvisi"
+    message2: "Pilih Bagian RPS yang akan direvisi",
   };
 
-
-  res.json(pesan)
-  console.log("Berhasil Merevisi RPS ")
-
+  res.json(pesan);
+  console.log("Berhasil Merevisi RPS ");
 });
 
 // Poin 9
@@ -154,18 +162,39 @@ app.get("/dosen/referensi/hapus", (req, res) => {
 });
 
 // Poin 15
-app.get("/dosen/nilai/tambah", (req, res) => {
-  res.send("Halaman dosen menambah nilai - poin 15");
+app.post("/dosen/nilai/tambah", (req, res) => {
+  let pesan = {
+    message: "Tambahkan Komponen Penilaian :",
+    Kode: "JSI62125",
+    Nama: "Pemrograman Web",
+    Komponen: ["1. Tugas =  15%", "2. Kehadiran = 5%", "3. UTS = 35%", "4. UAS = 45%"],
+  };
+  res.json(pesan);
+  console.log("Komponen penilaian berhasil ditambahkan");
 });
 
 // Poin 16
-app.get("/dosen/nilai/edit", (req, res) => {
-  res.send("Halaman dosen mmengedit nilai - poin 16");
+app.put("/dosen/nilai/edit", (req, res) => {
+  let pesan = {
+    message: "Ubah Komponen Penilaian :",
+    Kode: "JSI62125",
+    Nama: "Pemrograman Web",
+    Komponen: ["1. Tugas =  15%", "2. Kehadiran = 5%", "3. Keaktifan = 5%", "3. UTS = 35%", "4. UAS = 40%"],
+  };
+  res.json(pesan);
+  console.log("Komponen penilaian berhasil diubah");
 });
 
 // Poin 17
-app.get("/dosen/nilai/tambah", (req, res) => {
-  res.send("Halaman dosen menghapus nilai - poin - poin 17");
+app.delete("/dosen/nilai/hapus", (req, res) => {
+  let pesan = {
+    message: "Ubah Komponen Penilaian :",
+    Kode: "JSI62125",
+    Nama: "Pemrograman Web",
+    Komponen: ["1. Tugas =  15%", "3. Keaktifan = 10%", "3. UTS = 35%", "4. UAS = 40%"],
+  };
+  res.json(pesan);
+  console.log("Komponen penilaian berhasil dihapus");
 });
 
 // Poin 18
