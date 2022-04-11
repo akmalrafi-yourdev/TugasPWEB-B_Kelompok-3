900f64d32b928be483f840fe23128801299f70ce
Alif Abdul Rauf <2011522024_alif@student.unand.ac.id>
2022-04-11 19:24:49 +0700

    Koneksi DataBase

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'loki'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
