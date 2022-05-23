26c8c01747f41d21b63891b26b67da004726df9f
Alif Abdul Rauf <2011522024_alif@student.unand.ac.id>
2022-05-23 13:46:29 +0700

    admin

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -20,5 +20,5 @@
 //       console.log("1 record inserted");
 //     });
 // });
-
-  
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
