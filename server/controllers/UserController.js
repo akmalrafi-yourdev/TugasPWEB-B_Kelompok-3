18b5c3035142e1e2ade3c59df32c3edf628d60d8
Akmal Rafi Nursyabani <akmalrafi.yourdev@gmail.com>
2022-06-22 15:21:08 +0700

    sequelize get all user docs

---
+++ server/controllers/UserController.js
@@ -0,0 +1,11 @@
+import User from '../models/User.js';
+
+export const getUser = async (req, res) => {
+    try {
+        const user = await User.findAll();
+        res.send(user);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
