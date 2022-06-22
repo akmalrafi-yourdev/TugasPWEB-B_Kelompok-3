3d6bb54a344c3e2f7a083f0253ef5d9fe09d4231
Hafiz2028 <91659690+Hafiz2028@users.noreply.github.com>
2022-06-22 17:04:35 +0700

    models

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -8,4 +8,3 @@ export const getUser = async (req, res) => {
         console.log(err);
     }
 }
-
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUser } from "../controllers/UserController.js";
+import { getUser,register } from "../controllers/UserController.js";
 
 const router = express.Router();
 
