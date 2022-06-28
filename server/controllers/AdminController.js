7c344966265ca4c1fdb472d854756f887f66491a
Akmal Rafi Nursyabani <akmalrafi.yourdev@gmail.com>
2022-06-28 16:40:00 +0700

    commit

--- server/controllers/AdminController.js
+++ server/controllers/AdminController.js
@@ -7,12 +7,6 @@ const Sequelize = require("sequelize");
 
 const inputCoursePlanLecturers = async (req, res) => {
   try {
-    // const data = await DosenPengampu.create({
-    //     course_plan_id: req.params.course_plan_id,
-    //     lecturer_id: req.params.lecturer_id,
-    //     creator: req.params.creator,
-    //     // created_at: Sequelize.DataTypes.DATE
-    // })
     const data = await DosenPengampu.create(req.body);
     res.json({
       message: "data berhasil diinput! :D",
