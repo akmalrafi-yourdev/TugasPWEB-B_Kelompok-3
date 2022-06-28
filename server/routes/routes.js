6e8dc6fd2822660bb2d46f96d7b38ce0d4e4ad79
Hafiz2028 <91659690+Hafiz2028@users.noreply.github.com>
2022-06-28 16:24:31 +0700

    hehe

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Admin = require("../controllers/AdminController.js");
 const UserController = require("../controllers/UserController.js");
+const DosenController = require("../controllers/DosenController.js")
 // import { createCPMK, createKomponen, createPertemuan, createReferensi, createRPS, deleteCPMK, deleteKomponen, deletePertemuan, deleteReferensi, getCPMK, getDetailRPS, getKomponen, getPertemuan, getReferensi, getRPS, updateCPMK, updateKomponen, updatePertemuan, updateReferensi, updateRPS } from "../controllers/DosenController.js";
 const User = require("../controllers/UserController.js");
 // import detailRPS from "../models/DetailRPS.js";
@@ -14,26 +15,26 @@ const router = express.Router();
 
 //Dosen/Lecturer Routing
 
-// router.get('/rps', getRPS)
-// router.get('/detail-rps', getDetailRPS)
-// router.post('/rps', createRPS)
-// router.put('/rps/:id', updateRPS)
-// router.get('/cpmk', getCPMK)
-// router.post('/cpmk', createCPMK)
-// router.put('/cpmk', updateCPMK)
-// router.delete('/cpmk', deleteCPMK)
-// router.get('/referensi', getReferensi)
-// router.post('/referensi', createReferensi)
-// router.put('/referensi', updateReferensi)
-// router.delete('/referensi', deleteReferensi)
-// router.get('/komponen', getKomponen)
-// router.post('/komponen', createKomponen)
-// router.put('/komponen', updateKomponen)
-// router.delete('/komponen', deleteKomponen)
-// router.get('/pertemuan', getPertemuan)
-// router.post('/pertemuan', createPertemuan)
-// router.put('/pertemuan', updatePertemuan)
-// router.delete('/pertemuan', deletePertemuan)
+router.get('/dosen/rps', DosenController.getRPS)
+router.get('/dosen/detail-rps', DosenController.getDetailRPS)
+router.post('/dosen/rps', DosenController.createRPS)
+rDouter.put('/dosen/rps/:id', DosenController.updateRPS)
+router.get('/dosen/cpmk', DosenController.getCPMK)
+router.post('/dosen/cpmk', DosenController.createCPMK)
+router.put('/dosen/cpmk', DosenController.updateCPMK)
+router.delete('/dosen/cpmk', DosenController.deleteCPMK)
+router.get('/dosen/referensi', DosenController.getReferensi)
+router.post('/dosen/referensi', DosenController.createReferensi)
+router.put('/dosen/referensi', DosenController.updateReferensi)
+router.delete('/dosen/referensi', DosenController.deleteReferensi)
+router.get('/dosen/komponen', DosenController.getKomponen)
+router.post('/dosen/komponen', DosenController.createKomponen)
+router.put('/dosen/komponen', DosenController.updateKomponen)
+router.delete('/dosen/komponen', DosenController.deleteKomponen)
+router.get('/dosen/pertemuan', DosenController.getPertemuan)
+router.post('/dosen/pertemuan', DosenController.createPertemuan)
+router.put('/dosen/pertemuan', DosenController.updatePertemuan)
+router.delete('/dosen/pertemuan', DosenController.deletePertemuan)
 
 // USER/MAHASISWA ROUTES
 router.get('/user/matakuliah', User.getMatkul)
