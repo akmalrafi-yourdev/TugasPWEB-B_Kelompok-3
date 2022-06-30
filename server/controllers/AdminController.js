d101ab4a220f4acb44bfca99637c4e5e5de4d345
Akmal Rafi Nursyabani <akmalrafi.yourdev@gmail.com>
2022-06-30 10:10:54 +0700

    admin functionality update: list rps

--- server/controllers/AdminController.js
+++ server/controllers/AdminController.js
@@ -18,21 +18,21 @@ const inputCoursePlanLecturers = async (req, res) => {
 // Create Matkul
 const createMatkul = async (req, res) => {
   try {
-      await MataKuliah.create(req.body);
-      res.json({
-          "message": "MatKul Created"
-      });
+    await MataKuliah.create(req.body);
+    res.json({
+      message: "MatKul Created",
+    });
   } catch (err) {
-      console.log(err);
+    console.log(err);
   }
-}
+};
 
 const updateCoursePlanLecturers = async (req, res) => {
   try {
     await DosenPengampu.update(req.body, {
       where: {
         id: req.params.id,
-      }
+      },
     });
     res.json({
       message: "data berhasil diupdate! :D",
@@ -42,4 +42,27 @@ const updateCoursePlanLecturers = async (req, res) => {
   }
 };
 
-module.exports = { inputCoursePlanLecturers, updateCoursePlanLecturers, createMatkul };
+const listRPS = async (req, res) => {
+  try {
+    const rps = await IndexRPS.rps.findAll({
+      where: {
+        course_id: req.params.id,
+      },
+      include: [
+        {
+          model: IndexRPS.detailRPS,
+        },
+      ],
+    });
+    res.send(rps);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  inputCoursePlanLecturers,
+  updateCoursePlanLecturers,
+  createMatkul,
+  listRPS,
+};
