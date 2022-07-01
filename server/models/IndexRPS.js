7aa908a5cb04b5017dbf5df18535492ddccee297
Akmal Rafi Nursyabani <akmalrafi.yourdev@gmail.com>
2022-07-01 10:48:29 +0700

    commit

--- server/models/IndexRPS.js
+++ server/models/IndexRPS.js
@@ -6,6 +6,7 @@ const detailRPS = require("../models/DetailRPS");
 const cpmk = require("./cpmk");
 const mataKuliah = require("../models/MataKuliah");
 const References = require("./Referensi");
+const DosenPengampu = require("./DosenPengampu");
 
 rps.hasMany(detailRPS, { foreignKey: "course_plan_id" });
 detailRPS.belongsTo(rps, { foreignKey: "id" });
@@ -13,7 +14,10 @@ detailRPS.belongsTo(rps, { foreignKey: "id" });
 rps.hasMany(References, { foreignKey: "course_plan_id" });
 References.belongsTo(rps, { foreignKey: "id" });
 
+rps.hasMany(DosenPengampu, { foreignKey: "course_plan_id" });
+DosenPengampu.belongsTo(rps, { foreignKey: "id" });
+
 // rps.hasMany(cpmk, { foreignKey: "course_plan_id" });
 // cpmk.belongsTo(rps, { foreignKey: "id"});
 
-module.exports = { rps, detailRPS, mataKuliah, cpmk, References };
+module.exports = { rps, detailRPS, mataKuliah, cpmk, References, DosenPengampu };
